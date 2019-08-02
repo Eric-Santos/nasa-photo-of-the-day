23e8f1aa7d717998d02c05e82ecd7a55c1d8fb16
eric <Eric355402@yahoo.com>
2019-08-01 22:26:31 -0600

    mvp complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React /* useState, useEffect*/ from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import NasaImgList from './components/NasaImgList';
+import axios from 'axios';
+import Card from './Card';
 
 function App() {
-  // const [nasaPic, setNasaPic] = useState('');
-  // useEffect(() => {}, []);
+  const [nasaState, setNasaState] = useState({});
+
+  useEffect(() => {
+    axios
+      .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(response => setNasaState(response.data))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
-      <NasaImgList />
+    <div>
+      <Card nasaState={nasaState} />
     </div>
   );
 }
--- src/components/NasaImgListCard.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-//({imgUrl}) <-- references an object key, instead of using ()(props) return {props.imgUrl} vv
-export default function NasaImgListCard({ imgUrl }) {
-  return <img src={imgUrl} alt="nasa of the day" />;
-}
